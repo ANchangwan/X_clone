0f00ba0feead280f63b83f6c24a32486f5d13d28
안창완 <ssdii66@gmail.com>
2024-05-22 13:39:44 +0900

    firebase

---
+++ src/api/api.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDY7rZ8gi9XBMwzp3v7neNE9NRKFESdhDA",
+  authDomain: "x-reloaded-94b78.firebaseapp.com",
+  projectId: "x-reloaded-94b78",
+  storageBucket: "x-reloaded-94b78.appspot.com",
+  messagingSenderId: "349121080161",
+  appId: "1:349121080161:web:7275e444df68fca2787320",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
