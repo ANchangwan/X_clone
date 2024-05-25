e626301262817b0c29abd6bc9c32159f5b4bdaaf
안창완 <ssdii66@gmail.com>
2024-05-25 14:56:32 +0900

    log in

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,81 @@
-export default function Login() {
-  return <h1>Login</h1>;
+import { useState } from "react";
+import styled from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate, Link } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  Error,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+  Form,
+} from "../components/auth-components";
+
+export default function CreateAccount() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    if (isLoading || email === "" || password === "") return;
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+    //create an Account
+    //set thie name of the user
+    //redirect
+  };
+  return (
+    <Wrapper>
+      <Title>Login 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <Input
+          onChange={onChange}
+          name="password"
+          value={password}
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <Input type="submit" value={isLoading ? "Loading" : "Log In"} />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Don't have an account?<Link to="/create-account"> Create one</Link>
+      </Switcher>
+    </Wrapper>
+  );
 }
